305ae4bce00d2dd0cb650689b0a7608f003e75e2
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-01 02:04:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(num)
+  val=""
+  count=0
+if (num<2)
+	return false
+else
+	 i=2
+	while(i<num)
+		if (num%i==0)
+			count+=1
+		end
+		i+=1
+	end
+end
+	if count>=1
+		return false
+	else
+		return true
+	end
+	end
